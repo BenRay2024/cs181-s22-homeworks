0c2bcce0c1dbee444617030fb0f5df4c2017ae3e
Ben Ray <benray@college.harvard.edu>
2022-03-27 14:39:44 -0400

    HW4: Q1

--- HW4/T4_P1.py
+++ HW4/T4_P1.py
@@ -6,10 +6,13 @@ x_transform_neg = []
 y_transform_pos = []
 y_transform_neg = []
 
-def transform():
+def transform(part):
     for pair in data:
         x,y = pair
-        new_y = -(8/3) * (x ** 2) + (2/3) * (x ** 4)
+        if part == 1:
+            new_y = -(8/3) * (x ** 2) + (2/3) * (x ** 4)
+        else:
+            new_y = -4 * (x ** 2) + (1/2) * (x ** 4)
 
         if y == 1:
             x_transform_pos.append(x)
@@ -18,7 +21,7 @@ def transform():
             x_transform_neg.append(x)
             y_transform_neg.append(new_y)
 
-transform()
+transform(1)
 
 plt.scatter(x_transform_pos, y_transform_pos)
 plt.scatter(x_transform_neg, y_transform_neg)
@@ -32,4 +35,18 @@ ax.spines['bottom'].set_position('zero')
 ax.spines['top'].set_color('none')
 # max margin classifier
 plt.axhline(y=-1, color='r', linestyle='-')
+plt.show()
+
+transform(8)
+
+plt.scatter(x_transform_pos, y_transform_pos)
+plt.scatter(x_transform_neg, y_transform_neg)
+plt.ylim([-10, 10])
+plt.grid()
+# center y-axis
+ax = plt.gca()
+ax.spines['left'].set_position('zero')
+ax.spines['right'].set_color('none')
+ax.spines['bottom'].set_position('zero')
+ax.spines['top'].set_color('none')
 plt.show()
